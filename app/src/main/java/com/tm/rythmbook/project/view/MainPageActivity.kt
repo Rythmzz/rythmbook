500234e019361f4089ca19c06c0369ac5572c98e
Rythmzz <trantuminh968@gmail.com>
2023-06-07 09:47:45 +0700

    TM-STATUS: update activity About

--- app/src/main/java/com/tm/rythmbook/project/view/MainPageActivity.kt
+++ app/src/main/java/com/tm/rythmbook/project/view/MainPageActivity.kt
@@ -1,5 +1,6 @@
 package com.tm.rythmbook.project.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -12,17 +13,15 @@ class MainPageActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainPageBinding;
 
     private val viewModel: MyViewModel by viewModel()
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnAdd.setOnClickListener {
             viewModel.fetchName("Hai Tac")
+            val intent:Intent = Intent(this, AboutPageActivity::class.java)
+            startActivity(intent)
         }
         viewModel.myName.observe(this, Observer { newValue -> binding.tvHello.setText(newValue.toString()) })
-
     }
 }
